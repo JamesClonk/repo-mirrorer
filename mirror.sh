0f100962dfc32b5a15da6cc73ba3f98e007832a8
JamesClonk <jamesclonk@jamesclonk.ch>
2020-08-14 19:32:43 +0200

    mirror repos

--- mirror.sh
+++ mirror.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
+set -e
 
-# # collect jcio repos
-# curl -s https://api.github.com/orgs/jamesclonk-io/repos | jq -r .[].html_url > jcio.txt
+if [ -z "${GITLAB_TOKEN}" ]; then
+	echo "GITLAB_TOKEN is missing"
+	exit 1
+fi
 
-# # collect jamesclonk repos
-# curl -s 'https://api.github.com/users/JamesClonk/repos?per_page=100&page=1' | jq -r .[].html_url > jamesclonk.txt
-# curl -s 'https://api.github.com/users/JamesClonk/repos?per_page=100&page=2' | jq -r .[].html_url >> jamesclonk.txt
-# curl -s 'https://api.github.com/users/JamesClonk/repos?per_page=100&page=3' | jq -r .[].html_url >> jamesclonk.txt
-# curl -s 'https://api.github.com/users/JamesClonk/repos?per_page=100&page=4' | jq -r .[].html_url >> jamesclonk.txt
-# curl -s 'https://api.github.com/users/JamesClonk/repos?per_page=100&page=5' | jq -r .[].html_url >> jamesclonk.txt
+# collect jcio repos
+curl -s https://api.github.com/orgs/jamesclonk-io/repos | jq -r .[].html_url > jcio.txt
+
+# collect jamesclonk repos
+curl -s 'https://api.github.com/users/JamesClonk/repos?per_page=100&page=1' | jq -r .[].html_url > jamesclonk.txt
+curl -s 'https://api.github.com/users/JamesClonk/repos?per_page=100&page=2' | jq -r .[].html_url >> jamesclonk.txt
+curl -s 'https://api.github.com/users/JamesClonk/repos?per_page=100&page=3' | jq -r .[].html_url >> jamesclonk.txt
+curl -s 'https://api.github.com/users/JamesClonk/repos?per_page=100&page=4' | jq -r .[].html_url >> jamesclonk.txt
+curl -s 'https://api.github.com/users/JamesClonk/repos?per_page=100&page=5' | jq -r .[].html_url >> jamesclonk.txt
 
 # clone jcio repos
-for repo in $(cat jcio.txt); do
+mkdir -p jcio || true
+pushd jcio
+for repo in $(cat ../jcio.txt); do
+	rm -rf repo || true
 	echo "cloning ${repo} ..."
-	echo "git clone ${repo}"
+	git clone ${repo} repo
+	pushd repo
+		GITLAB_URL=$(git remote get-url origin | sed "s/github.com/oauth2:${GITLAB_TOKEN}@gitlab.com/g") # https://oauth2:TOKEN@gitlab.com/${repo_org}/${repo_name}.git
+		git remote add gitlab "${GITLAB_URL}.git" || git remote set-url gitlab "${GITLAB_URL}.git"
+		echo "pushing to gitlab ..."
+		git push -f gitlab
+	popd
 done
+popd
+
+# clone jamesclonk repos
+mkdir -p jamesclonk || true
+pushd jamesclonk
+for repo in $(cat ../jamesclonk.txt); do
+	rm -rf repo || true
+	echo "cloning ${repo} ..."
+	git clone ${repo} repo
+	pushd repo
+		GITLAB_URL=$(git remote get-url origin | sed "s/github.com/oauth2:${GITLAB_TOKEN}@gitlab.com/g") # https://oauth2:TOKEN@gitlab.com/${repo_org}/${repo_name}.git
+		git remote add gitlab "${GITLAB_URL}.git" || git remote set-url gitlab "${GITLAB_URL}.git"
+		echo "pushing to gitlab ..."
+		git push -f gitlab
+	popd
+done
+popd
