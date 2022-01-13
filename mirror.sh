00f502978a0bc2ee95ab5bc0fac4effc108ea445
JamesClonk <jamesclonk@jamesclonk.ch>
2022-01-13 12:45:11 +0100

    use github token

--- mirror.sh
+++ mirror.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ -z "${GITHUB_TOKEN}" ]; then
+	echo "GITHUB_TOKEN is missing"
+	exit 1
+fi
 if [ -z "${GITLAB_TOKEN}" ]; then
 	echo "GITLAB_TOKEN is missing"
 	exit 1
@@ -8,22 +12,22 @@ fi
 
 # collect jcio repos
 echo "collecting [jamesclonk-io] repos ..."
-curl -H 'Accept: application/vnd.github.v3+json' -s \
-	'https://api.github.com/orgs/jamesclonk-io/repos' | jq -r .[].html_url > jcio.txt
+curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
+	'https://api.github.com/orgs/jamesclonk-io/repos' | jq -r '.[].html_url' > jcio.txt
 echo "$(cat jcio.txt | wc -l) repos found."
 
 # collect jamesclonk repos
 echo "collecting [JamesClonk] repos ..."
-curl -H 'Accept: application/vnd.github.v3+json' -s \
-	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=1' | jq -r .[].html_url > jamesclonk.txt
-curl -H 'Accept: application/vnd.github.v3+json' -s \
-	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=2' | jq -r .[].html_url >> jamesclonk.txt
-curl -H 'Accept: application/vnd.github.v3+json' -s \
-	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=3' | jq -r .[].html_url >> jamesclonk.txt
-curl -H 'Accept: application/vnd.github.v3+json' -s \
-	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=4' | jq -r .[].html_url >> jamesclonk.txt
-curl -H 'Accept: application/vnd.github.v3+json' -s \
-	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=5' | jq -r .[].html_url >> jamesclonk.txt
+curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
+	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=1' | jq -r '.[].html_url' > jamesclonk.txt
+curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
+	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=2' | jq -r '.[].html_url' >> jamesclonk.txt
+curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
+	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=3' | jq -r '.[].html_url' >> jamesclonk.txt
+curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
+	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=4' | jq -r '.[].html_url' >> jamesclonk.txt
+curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
+	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=5' | jq -r '.[].html_url' >> jamesclonk.txt
 echo "$(cat jamesclonk.txt | wc -l) repos found."
 
 # clone jcio repos
