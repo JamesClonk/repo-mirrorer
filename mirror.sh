83548d8c17618a57d377467eea8e34264be3dde4
JamesClonk <jamesclonk@jamesclonk.ch>
2023-04-09 13:15:56 +0200

    also clone private repos

--- mirror.sh
+++ mirror.sh
@@ -40,15 +40,15 @@ popd
 echo "collecting [JamesClonk] repos ..."
 rm -f /tmp/jamesclonk.txt || true
 curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
-	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=1' | jq -r '.[].html_url' > /tmp/jamesclonk.txt
+	'https://api.github.com/user/repos?per_page=100&page=1&visibility=all&affiliation=owner' | jq -r '.[].html_url' > /tmp/jamesclonk.txt
 curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
-	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=2' | jq -r '.[].html_url' >> /tmp/jamesclonk.txt
+	'https://api.github.com/user/repos?per_page=100&page=2&visibility=all&affiliation=owner' | jq -r '.[].html_url' >> /tmp/jamesclonk.txt
 curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
-	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=3' | jq -r '.[].html_url' >> /tmp/jamesclonk.txt
+	'https://api.github.com/user/repos?per_page=100&page=3&visibility=all&affiliation=owner' | jq -r '.[].html_url' >> /tmp/jamesclonk.txt
 curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
-	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=4' | jq -r '.[].html_url' >> /tmp/jamesclonk.txt
+	'https://api.github.com/user/repos?per_page=100&page=4&visibility=all&affiliation=owner' | jq -r '.[].html_url' >> /tmp/jamesclonk.txt
 curl -H 'Accept: application/vnd.github.v3+json' -s -H "Authorization: token ${GITHUB_TOKEN}" \
-	'https://api.github.com/users/JamesClonk/repos?per_page=100&page=5' | jq -r '.[].html_url' >> /tmp/jamesclonk.txt
+	'https://api.github.com/user/repos?per_page=100&page=5&visibility=all&affiliation=owner' | jq -r '.[].html_url' >> /tmp/jamesclonk.txt
 echo "$(cat /tmp/jamesclonk.txt | wc -l) repos found."
 
 # clone jamesclonk repos
@@ -57,9 +57,10 @@ pushd /tmp/jamesclonk
 for repo in $(cat /tmp/jamesclonk.txt); do
 	rm -rf repo || true
 	echo "cloning ${repo} ..."
-	git clone ${repo} repo
+	GITHUB_URL=$(echo "${repo}" | sed "s/github.com/JamesClonk:${GITHUB_TOKEN}@github.com/g")
+	git clone ${GITHUB_URL} repo
 	pushd repo
-		GITLAB_URL=$(git remote get-url origin | sed "s/github.com/oauth2:${GITLAB_TOKEN}@gitlab.com/g") # https://oauth2:TOKEN@gitlab.com/${repo_org}/${repo_name}.git
+		GITLAB_URL=$(echo "${repo}" | sed "s/github.com/oauth2:${GITLAB_TOKEN}@gitlab.com/g") # https://oauth2:TOKEN@gitlab.com/${repo_org}/${repo_name}.git
 		git remote add gitlab "${GITLAB_URL}.git" || git remote set-url gitlab "${GITLAB_URL}.git"
 		echo "pushing to gitlab ..."
 		git push -f gitlab
